69212b14393c736992fb0fd3d15a2e11805346a7
MrBStones <70972115+MrBStones@users.noreply.github.com>
2026-04-20 20:24:25 +0200

    Add prototype images for monsters

--- constants/monsters.ts
+++ constants/monsters.ts
@@ -2,47 +2,47 @@ import { Monster } from "@/types/monsterType";
 
 export const MonsterLevel1: Monster = {
   value: 1,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon1.png"),
 };
 
 export const MonsterLevel2: Monster = {
   value: 2,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon2.png"),
 };
 
 export const MonsterLevel3: Monster = {
   value: 3,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon3.png"),
 };
 
 export const MonsterLevel4: Monster = {
   value: 4,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon4.png"),
 };
 
 export const MonsterLevel5: Monster = {
   value: 5,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon5.png"),
 };
 
 export const MonsterLevel6: Monster = {
   value: 6,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon6.png"),
 };
 
 export const MonsterLevel7: Monster = {
   value: 7,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon7.png"),
 };
 
 export const MonsterLevel8: Monster = {
   value: 8,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon8.png"),
 };
 
 export const MonsterLevel9: Monster = {
   value: 9,
-  imgSource: require("@/assets/images/icon.png"),
+  imgSource: require("@/assets/images/mon9.png"),
 };
 
 export const MonstersArray: Monster[] = [
